8d172df829172b09675cbb1c31e6262aefbe3ef7
Carlos1681 <spjcarlosh@gmail.com>
2024-07-03 19:50:22 -0300

    Initial commit

---
+++ src/main/java/project/dio/SpringApiDioApplication.java
@@ -0,0 +1,13 @@
+package project.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringApiDioApplication.class, args);
+	}
+
+}
