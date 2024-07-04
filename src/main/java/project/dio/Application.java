4d7c2128bd619df489dfbe52374fc5f3e1f43d75
Carlos1681 <spjcarlosh@gmail.com>
2024-07-03 21:00:22 -0300

    Implementação da camada de dominio

--- src/main/java/project/dio/Application.java
+++ src/main/java/project/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringApiDioApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringApiDioApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
